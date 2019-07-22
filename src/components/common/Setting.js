5959265b3ec8467df9c0000e6964f130494a6d6b
Ludhiya <ludhiyavjose1@gmail.com>
2019-07-22 13:14:10 +0530

    added setting menu ui

--- src/components/common/Setting.js
+++ src/components/common/Setting.js
@@ -1,9 +1,54 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
 import Menu from "@material-ui/core/Menu";
 import MenuItem from "@material-ui/core/MenuItem";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
+import Button from "@material-ui/core/Button";
+import Slider from "@material-ui/core/Slider";
+
+const useStyles = makeStyles(theme => ({
+  btn: {
+    marginRight: theme.spacing(1),
+    display: "inline-block",
+    textTransform: "none",
+    textAlign: "center",
+    fontSize: 16
+  },
+  serif: {
+    fontFamily: '"Roboto Slab", "serif"',
+    textTransform: "none",
+    fontSize: 16
+  },
+  sans: {
+    fontFamily: '"Roboto", "sans-serif"',
+    textTransform: "none",
+    fontSize: 16
+  },
+  menu: {
+    textAlign: "center",
+    width: "100%",
+    display: "inline-block",
+    fontSize: 18
+  },
+  margin: {
+    height: theme.spacing(5)
+  },
+  slider: {
+    color: "#bfbfbf"
+  }
+}));
 const ITEM_HEIGHT = 68;
 const Setting = ({ settingsAnchor, handleClose }) => {
+  const classes = useStyles();
   const open = Boolean(settingsAnchor);
+  function valuetext(value) {
+    return `${value}°C`;
+  }
+
+  // function valueLabelFormat(value) {
+  //   return marks.findIndex(mark => mark.value === value) + 1;
+  // }
+
   return (
     <>
       <Menu
@@ -15,14 +60,38 @@ const Setting = ({ settingsAnchor, handleClose }) => {
         PaperProps={{
           style: {
             maxHeight: ITEM_HEIGHT * 4.5,
-            width: 130,
+            width: 250,
             backgroundColor: "#5181a9",
             color: "#fff"
           }
         }}
       >
-        <MenuItem onClick={handleClose}>Download</MenuItem>
-        <MenuItem onClick={handleClose}>Print</MenuItem>
+        <MenuItem className={classes.menu}>Font Family</MenuItem>
+        <MenuItem className={classes.menu} onClick={handleClose}>
+          <ButtonGroup
+            variant="contained"
+            aria-label="Large contained secondary button group"
+          >
+            <Button className={classes.sans}>Sans</Button>
+            <Button className={classes.serif}>Serif</Button>
+          </ButtonGroup>
+        </MenuItem>
+
+        <MenuItem className={classes.menu}>Font Size</MenuItem>
+        <MenuItem className={classes.menu}>
+          <div className={classes.margin} />
+          <Slider
+            defaultValue={16}
+            getAriaValueText={valuetext}
+            aria-labelledby="discrete-slider-always"
+            step={2}
+            marks
+            valueLabelDisplay="on"
+            min={12}
+            max={20}
+            classes={{ root: classes.slider }}
+          />
+        </MenuItem>
       </Menu>
     </>
   );
--- src/components/read/Bible.js
+++ src/components/read/Bible.js
@@ -4,8 +4,11 @@ import Fab from "@material-ui/core/Fab";
 import { makeStyles } from "@material-ui/styles";
 import KeyboardArrowLeft from "@material-ui/icons/KeyboardArrowLeft";
 import KeyboardArrowRight from "@material-ui/icons/KeyboardArrowRight";
+import Grid from "@material-ui/core/Grid";
+
 const useStyles = makeStyles(theme => ({
-  bible: {
+  root: {
+    flexGrow: 1,
     padding: "25px 8%",
     backgroundColor: "white",
     lineHeight: 2,
@@ -13,11 +16,13 @@ const useStyles = makeStyles(theme => ({
     "& p": {
       fontSize: 16,
       textAlign: "justify",
-      // fontFamily: "Istok Web",
+
       color: "#616161"
-      // fontWeight: 200
     }
   },
+  // bible: {
+
+  // },
   margin: {
     position: "fixed",
     top: "50vh",
@@ -33,92 +38,100 @@ const useStyles = makeStyles(theme => ({
 const Bible = () => {
   const classes = useStyles();
   return (
-    // eslint-disable-next-line react/jsx-no-undef
-    <div className={classes.bible}>
-      <h5>The Wise Men Visit</h5>
-      <p>
-        2 Jesus was born in Bethlehem in Judea when Herod was king. After Jesus’
-        birth wise men[a] from the east arrived in Jerusalem. 2 They asked,
-        “Where is the one who was born to be the king of the Jews? We saw his
-        star rising and have come to worship him.” 3 When King Herod and all
-        Jerusalem heard about this, they became disturbed. 4 He called together
-        all the chief priests and the experts in the Scriptures and tried to
-        find out from them where the Messiah was supposed to be born. 5 They
-        told him, “In Bethlehem in Judea. The prophet wrote about this: 6
-        Bethlehem in the land of Judah, you are by no means least among the
-        leaders of Judah. A leader will come from you. He will shepherd my
-        people Israel.” 7 Then Herod secretly called the wise men and found out
-        from them exactly when the star had appeared. 8 As he sent them to
-        Bethlehem, he said, “Go and search carefully for the child. When you
-        have found him, report to me so that I may go and worship him too.” 9
-        After they had heard the king, they started out. The star they had seen
-        rising led them until it stopped over the place where the child was. 10
-        They were overwhelmed with joy to see the star. 11 When they entered the
-        house, they saw the child with his mother Mary. So they bowed down and
-        worshiped him. Then they opened their treasure chests and offered him
-        gifts of gold, frankincense, and myrrh.[b] 12 God warned them in a dream
-        not to go back to Herod. So they left for their country by another road.
-        The Escape to Egypt 13 After they had left, an angel of the Lord
-        appeared to Joseph in a dream. The angel said to him, “Get up, take the
-        child and his mother, and flee to Egypt. Stay there until I tell you,
-        because Herod intends to search for the child and kill him.” 14 Joseph
-        got up, took the child and his mother, and left for Egypt that night. 15
-        He stayed there until Herod died. What the Lord had spoken through the
-        prophet came true: “I have called my son out of Egypt.” 16 When Herod
-        saw that the wise men had tricked him, he became furious. He sent
-        soldiers to kill all the boys two years old and younger in or near
-        Bethlehem. This matched the exact time he had learned from the wise men.
-        17 Then the words spoken through the prophet Jeremiah came true: 2 Jesus
-        was born in Bethlehem in Judea when Herod was king. After Jesus’ birth
-        wise men[a] from the east arrived in Jerusalem. 2 They asked, “Where is
-        the one who was born to be the king of the Jews? We saw his star rising
-        and have come to worship him.” 3 When King Herod and all Jerusalem heard
-        about this, they became disturbed. 4 He called together all the chief
-        priests and the experts in the Scriptures and tried to find out from
-        them where the Messiah was supposed to be born. 5 They told him, “In
-        Bethlehem in Judea. The prophet wrote about this: 6 Bethlehem in the
-        land of Judah, you are by no means least among the leaders of Judah. A
-        leader will come from you. He will shepherd my people Israel.” 7 Then
-        Herod secretly called the wise men and found out from them exactly when
-        the star had appeared. 8 As he sent them to Bethlehem, he said, “Go and
-        search carefully for the child. When you have found him, report to me so
-        that I may go and worship him too.” 9 After they had heard the king,
-        they started out. The star they had seen rising led them until it
-        stopped over the place where the child was. 10 They were overwhelmed
-        with joy to see the star. 11 When they entered the house, they saw the
-        child with his mother Mary. So they bowed down and worshiped him. Then
-        they opened their treasure chests and offered him gifts of gold,
-        frankincense, and myrrh.[b] 12 God warned them in a dream not to go back
-        to Herod. So they left for their country by another road. The Escape to
-        Egypt 13 After they had left, an angel of the Lord appeared to Joseph in
-        a dream. The angel said to him, “Get up, take the child and his mother,
-        and flee to Egypt. Stay there until I tell you, because Herod intends to
-        search for the child and kill him.” 14 Joseph got up, took the child and
-        his mother, and left for Egypt that night. 15 He stayed there until
-        Herod died. What the Lord had spoken through the prophet came true: “I
-        have called my son out of Egypt.” 16 When Herod saw that the wise men
-        had tricked him, he became furious. He sent soldiers to kill all the
-        boys two years old and younger in or near Bethlehem. This matched the
-        exact time he had learned from the wise men. 17 Then the words spoken
-        through the prophet Jeremiah came true:
-      </p>
+    <div className={classes.root}>
+      <Grid container>
+        <Grid item xs={12}>
+          {/* <div className={classes.bible}> */}
+          <h5>The Wise Men Visit</h5>
+          <p>
+            2 Jesus was born in Bethlehem in Judea when Herod was king. After
+            Jesus’ birth wise men[a] from the east arrived in Jerusalem. 2 They
+            asked, “Where is the one who was born to be the king of the Jews? We
+            saw his star rising and have come to worship him.” 3 When King Herod
+            and all Jerusalem heard about this, they became disturbed. 4 He
+            called together all the chief priests and the experts in the
+            Scriptures and tried to find out from them where the Messiah was
+            supposed to be born. 5 They told him, “In Bethlehem in Judea. The
+            prophet wrote about this: 6 Bethlehem in the land of Judah, you are
+            by no means least among the leaders of Judah. A leader will come
+            from you. He will shepherd my people Israel.” 7 Then Herod secretly
+            called the wise men and found out from them exactly when the star
+            had appeared. 8 As he sent them to Bethlehem, he said, “Go and
+            search carefully for the child. When you have found him, report to
+            me so that I may go and worship him too.” 9 After they had heard the
+            king, they started out. The star they had seen rising led them until
+            it stopped over the place where the child was. 10 They were
+            overwhelmed with joy to see the star. 11 When they entered the
+            house, they saw the child with his mother Mary. So they bowed down
+            and worshiped him. Then they opened their treasure chests and
+            offered him gifts of gold, frankincense, and myrrh.[b] 12 God warned
+            them in a dream not to go back to Herod. So they left for their
+            country by another road. The Escape to Egypt 13 After they had left,
+            an angel of the Lord appeared to Joseph in a dream. The angel said
+            to him, “Get up, take the child and his mother, and flee to Egypt.
+            Stay there until I tell you, because Herod intends to search for the
+            child and kill him.” 14 Joseph got up, took the child and his
+            mother, and left for Egypt that night. 15 He stayed there until
+            Herod died. What the Lord had spoken through the prophet came true:
+            “I have called my son out of Egypt.” 16 When Herod saw that the wise
+            men had tricked him, he became furious. He sent soldiers to kill all
+            the boys two years old and younger in or near Bethlehem. This
+            matched the exact time he had learned from the wise men. 17 Then the
+            words spoken through the prophet Jeremiah came true: 2 Jesus was
+            born in Bethlehem in Judea when Herod was king. After Jesus’ birth
+            wise men[a] from the east arrived in Jerusalem. 2 They asked, “Where
+            is the one who was born to be the king of the Jews? We saw his star
+            rising and have come to worship him.” 3 When King Herod and all
+            Jerusalem heard about this, they became disturbed. 4 He called
+            together all the chief priests and the experts in the Scriptures and
+            tried to find out from them where the Messiah was supposed to be
+            born. 5 They told him, “In Bethlehem in Judea. The prophet wrote
+            about this: 6 Bethlehem in the land of Judah, you are by no means
+            least among the leaders of Judah. A leader will come from you. He
+            will shepherd my people Israel.” 7 Then Herod secretly called the
+            wise men and found out from them exactly when the star had appeared.
+            8 As he sent them to Bethlehem, he said, “Go and search carefully
+            for the child. When you have found him, report to me so that I may
+            go and worship him too.” 9 After they had heard the king, they
+            started out. The star they had seen rising led them until it stopped
+            over the place where the child was. 10 They were overwhelmed with
+            joy to see the star. 11 When they entered the house, they saw the
+            child with his mother Mary. So they bowed down and worshiped him.
+            Then they opened their treasure chests and offered him gifts of
+            gold, frankincense, and myrrh.[b] 12 God warned them in a dream not
+            to go back to Herod. So they left for their country by another road.
+            The Escape to Egypt 13 After they had left, an angel of the Lord
+            appeared to Joseph in a dream. The angel said to him, “Get up, take
+            the child and his mother, and flee to Egypt. Stay there until I tell
+            you, because Herod intends to search for the child and kill him.” 14
+            Joseph got up, took the child and his mother, and left for Egypt
+            that night. 15 He stayed there until Herod died. What the Lord had
+            spoken through the prophet came true: “I have called my son out of
+            Egypt.” 16 When Herod saw that the wise men had tricked him, he
+            became furious. He sent soldiers to kill all the boys two years old
+            and younger in or near Bethlehem. This matched the exact time he had
+            learned from the wise men. 17 Then the words spoken through the
+            prophet Jeremiah came true:
+          </p>
 
-      <Fab
-        size="small"
-        color="default"
-        aria-label="Add"
-        className={classes.margin}
-      >
-        <KeyboardArrowLeft />
-      </Fab>
-      <Fab
-        size="small"
-        color="default"
-        aria-label="Add"
-        className={classes.right}
-      >
-        <KeyboardArrowRight />
-      </Fab>
+          <Fab
+            size="small"
+            color="default"
+            aria-label="Add"
+            classes={{ sizeSmall: classes.margin }}
+          >
+            <KeyboardArrowLeft />
+          </Fab>
+          <Fab
+            size="small"
+            color="default"
+            aria-label="Add"
+            classes={{ sizeSmall: classes.right }}
+          >
+            <KeyboardArrowRight />
+          </Fab>
+        </Grid>
+      </Grid>
     </div>
   );
 };
